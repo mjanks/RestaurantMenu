c5f831f5ceefa04e84ca0bb13e763ceb53b3de7c
Michael Janks <mjanks@emich.edu>
2022-01-20 21:33:27 -0500

    added more functionality

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -15,11 +15,51 @@ public class Menu {
         return lastUpdated;
     }
 
-    public void addMenuItem(MenuItem menuItem) {
-        menuItems.add(menuItem);
+    public void addMenuItem(MenuItem itemToAdd) {
+        menuItems.add(itemToAdd);
+    }
+
+    public void removeMenuItem(int id) {
+        for(int i=0; i < menuItems.size(); i++) {
+            if(menuItems.get(i).getId() == id) {
+                menuItems.remove(i);
+            }
+        }
+    }
+
+    public boolean isNew(int id) {
+        for(int i=0; i < menuItems.size(); i++) {
+            if(menuItems.get(i).getId() == id) {
+                return menuItems.get(i).isNew();
+            }
+        }
+        return false;
     }
 
     public void setLastUpdated(Date lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
+
+    public void printMenu() {
+        for(MenuItem item : menuItems) {
+            System.out.println("Id: " + item.getId() + " Description: " + item.getDescription() +
+                    " Price: " + item.getPrice());
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        MenuItem item1 = new MenuItem(3.23, "wings", "app", true);
+        MenuItem item2 = new MenuItem(5.23, "queso", "app", true);
+        MenuItem item3 = new MenuItem(6.50, "nachos", "app", false);
+        System.out.println(item1.getId());
+        System.out.println(item2.getId());
+        System.out.println(item3.getId());
+        Menu menu = new Menu();
+        menu.addMenuItem(item1);
+        menu.addMenuItem(item2);
+        menu.addMenuItem(item3);
+        menu.printMenu();
+    }
+
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,10 +1,25 @@
 package restaurant;
 
 public class MenuItem {
+    private static int id = 1;
+    private int menuId;
     private double price;
     private String description;
     private String category;
     private boolean isNew;
+
+    public MenuItem(double price, String description, String category, boolean isNew) {
+        this.menuId = id;
+        id++;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public int getId() {
+        return menuId;
+    }
 
     public double getPrice() {
         return price;
@@ -20,6 +35,10 @@ public class MenuItem {
 
     public boolean isNew() {
         return isNew;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setPrice(double price) {
